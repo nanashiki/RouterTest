8ddcc81382143f6daad582dfec01205abe52e005
nanashiki <nanashiki.app@gmail.com>
2020-03-31 00:08:42 +0900

    Initial Commit

---
+++ RouterTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouterTest
+//
+//  Created by nanashiki on 2020/03/31.
+//  Copyright © 2020 nanashiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
